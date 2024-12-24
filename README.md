269122c1ca8596a2aef801281ba28b4a30d8c35a
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2024-12-24 14:50:32 -0500

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio Template (Dev)
+
+By J. Blackburn 
+
+Last updated: Dec 24 2024 (**WIP**)
+
+***
+
+### description: 
+
+Simple, responsive, and customizable portfolio website template intended for up-and-coming programmers. Project uses express to serve simple HTML file locally, intended for development.
+
+### usage: 
+
+* install express  -  npm install
+
+* start local server  -  npm start
---
+++ server.js
@@ -0,0 +1,30 @@
+//
+// Created by J. Blackburn - Dec 24 2024
+//
+
+const express = require( 'express' );
+const path    = require( 'path'    );
+
+
+
+const port  = 5000;
+const root  = path.join(__dirname, 'public');
+
+const app   = express();
+
+
+app.use(  express.static( root )  );
+
+
+
+	// response is the same for every path
+
+app.get(  '*',  (req, res) => {
+	
+		// respond with ./public/index.html
+	res.sendFile(  path.join( root, 'index.html' )  );
+
+});
+
+
+app.listen(  port,  () => console.log('Server up and running.')  ); 
